3eed521d46aa4827ada678aa7bd118bd5875add5
walterblasquezcontreras <wa.blasquez@duocuc.cl>
2024-07-03 11:05:01 -0400

    listas

---
+++ evaluación_Blasquez.py
@@ -0,0 +1,53 @@
+import os
+from funciones import *
+import time
+import msvcrt
+
+Rut = []
+nombre = []
+direccion = []
+comuna = []
+cil_5 = []
+cil_15 = []
+
+
+
+while True:
+
+    
+
+    print("""
+    bienvenido a Gaxplosive.
+    ¿que desea hacer?: 
+         
+    (1). Registrar pedido
+    (2). Listar todos los pedidos
+    (3). Buscar pedido por RUT
+    (4). Imprimir hoja de ruta
+    (5). salir
+    """)
+    
+    msvcrt.getch
+    opc = int(input("elija la opcion que desea ejecutar: "))
+
+    if opc == 1:
+        pass
+    elif opc == 2:
+        pass
+    elif opc == 3:
+        pass
+    elif opc == 4:
+        pass
+    elif opc == 5:    
+        print("Muchas gracias, vuelva pronto. ")
+        break
+    else:
+        print("!ERROR, esa opcion no existe.")
+        break
+    
+   
+    
+    
+    
+
+
---
+++ funciones.py
@@ -0,0 +1,7 @@
+import os
+import msvcrt
+import time
+import csv
+
+
+
